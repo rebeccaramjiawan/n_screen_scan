7091645a1efdc6520ce1c61908e93e8dff9ea340
Rebecca Louise Ramjiawan <rebecca.louise.ramjiawan@cern.ch>
2022-12-16 13:26:40 +0100

    Tidying up

--- BTV_run.py
+++ BTV_run.py
@@ -3,39 +3,45 @@ import n_screen_scan.plotting as plot
 import n_screen_scan.BTV_calculations as btv_calc
 import configparser
 
-# Inputs sources from BTV_config.txt file
-config = configparser.RawConfigParser()
-config.read('n_screen_scan/BTV_config.txt')
 
-btvs = config['INPUT']["btvs"].split(",")
-btv_madx = config['INPUT']["btv_madx"].split(",")
-btv_screens = config['INPUT']["btv_screens"].split(",")
-particle = config['INPUT']["particle"]  # e (electrons) or p (protons)
-mass = float(config['INPUT']["mass"])
-energy = float(config['INPUT']["energy"])  # beam energy in MeV
-n_shot = int(config['INPUT']["n_shot"])  # number of shots to average
-n_btv_x = int(config['INPUT']["n_btv_x"])  # number of horizontal BTVs to use
-n_btv_y = int(config['INPUT']["n_btv_y"])  # number of vertical BTVs to use
-simulate = config['INPUT']["simulate"] == "True"  # if true then requires emitta and mom_spr to be set
-is_rms = config['INPUT']["is_rms"] == "True"  # False gives you standard deviation
-emitta_x = float(config['INPUT']["emitta_x"])  # Horizontal emittance
-emitta_y = float(config['INPUT']["emitta_y"])  # Vertical emittnce
-mom_spr = float(config['INPUT']["mom_spr"])  # Momentum spread
-gamma_r = np.sqrt(energy ** 2 + mass ** 2) / mass  # Lorentz gamma
+def main():
+    # Inputs sources from BTV_config.txt file
+    config = configparser.RawConfigParser()
+    config.read('n_screen_scan/BTV_config.txt')
 
-# Check number of BTVs match that requested
-if (n_btv_x > len(btv_madx)) or (n_btv_x > len(btv_madx)):
-    print("** Number of BTVs requested exceeds number of BTVs provided **")
-if len(btvs) != len(btv_madx):
-    print("** Number of BTVs does not equal number of screens **")
+    btvs = config['INPUT']["btvs"].split(",")
+    btv_madx = config['INPUT']["btv_madx"].split(",")
+    btv_screens = config['INPUT']["btv_screens"].split(",")
+    particle = config['INPUT']["particle"]  # e (electrons) or p (protons)
+    mass = float(config['INPUT']["mass"])
+    energy = float(config['INPUT']["energy"])  # beam energy in MeV
+    n_shot = int(config['INPUT']["n_shot"])  # number of shots to average
+    n_btv_x = int(config['INPUT']["n_btv_x"])  # number of horizontal BTVs to use
+    n_btv_y = int(config['INPUT']["n_btv_y"])  # number of vertical BTVs to use
+    simulate = config['INPUT']["simulate"] == "True"  # if true then requires emitta and mom_spr to be set
+    is_rms = config['INPUT']["is_rms"] == "True"  # False gives you standard deviation
+    emitta_x = float(config['INPUT']["emitta_x"])  # Horizontal emittance
+    emitta_y = float(config['INPUT']["emitta_y"])  # Vertical emittnce
+    mom_spr = float(config['INPUT']["mom_spr"])  # Momentum spread
+    gamma_r = np.sqrt(energy ** 2 + mass ** 2) / mass  # Lorentz gamma
 
-btv_madx = [x.lower() for x in btv_madx]
+    # Check number of BTVs match that requested
+    if (n_btv_x > len(btv_madx)) or (n_btv_x > len(btv_madx)):
+        print("** Number of BTVs requested exceeds number of BTVs provided **")
+    if len(btvs) != len(btv_madx):
+        print("** Number of BTVs does not equal number of screens **")
 
-btv_calc_inst = btv_calc.BTV_calc(energy, mass, emitta_x, emitta_y, mom_spr, btv_madx, btvs, btv_screens, simulate,
-                                  particle,
-                                  n_btv_x, n_btv_y)  # Calculation  methods in BTV_calc
-estimates, data_xy, twiss = btv_calc_inst.emit_calc(n_shot, is_rms)
+    btv_madx = [x.lower() for x in btv_madx]
 
-plot.plot_btv(btv_calc_inst, btv_madx, btvs, gamma_r, estimates,
-              data_xy)  # plot data from BTV with estimates for beam parameters
-plot.plot_twiss(btv_calc_inst, btv_madx, btvs, gamma_r, estimates, data_xy, twiss)  # propagate Twiss parameters
+    btv_calc_inst = btv_calc.BTV_calc(energy, mass, emitta_x, emitta_y, mom_spr, btv_madx, btvs, btv_screens, simulate,
+                                      particle,
+                                      n_btv_x, n_btv_y)  # Calculation  methods in BTV_calc
+    estimates, data_xy, twiss = btv_calc_inst.emit_calc(n_shot, is_rms)
+
+    plot.plot_btv(btv_calc_inst, btv_madx, btvs, gamma_r, estimates,
+                  data_xy)  # plot data from BTV with estimates for beam parameters
+    plot.plot_twiss(btv_calc_inst, btv_madx, btvs, gamma_r, estimates, data_xy, twiss)  # propagate Twiss parameters
+
+
+if __name__ == "__main__":
+    main()
--- README.md
+++ README.md
@@ -3,11 +3,13 @@
 This is code for the calculation of beam paramters such as the emittance, dispersion and Twiss parameters
 from BTV data (3+ BTVs required). A BTV is a beam screen, it captures a 2D (x-y) image of the beam as it passes. We need to understand the beam in 6D.
 
-By combining the BTV data with a simulation of the beamline we can reconstruct the initial beam parameters at the start of the beamline. 
+By combining the BTV data with a simulation of the beamline we can reconstruct higher-dimensional initial beam parameters (at the start of the beamline). 
 To have an accurate model of the beam, it is essential to regularly measure the beam parameters because they can vary from hour-to-hour.
 
 This code pulls the current beamline magnet settings from Git (requires being at CERN for this functionality), takes data from the BTVs (n shots per BTV),
 and uses a simulation of the beamline to model the beam dynamics between the BTVs. Beam propagation through magnets and drifts are modelled using 6D matrixes quantified in the code by the parameters R**. 
+
+From the plots below, the data show good agreement in the horizontal plane with the model but very poor agreement in the vertical plane. Still on-going to figure out why!
 
 ## Run BTV_run.py
 
